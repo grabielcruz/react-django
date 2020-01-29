d7382745f7734472d44b9f2f6f6b145036c4b219
grabielcruz <luisgrabielcruz@gmail.com>
2020-01-29 14:43:26 -0400

    tutorial ended

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -8,8 +8,7 @@ export class Alerts extends Component {
   static propTypes = {
     error: PropTypes.object.isRequired,
     message: PropTypes.object.isRequired
-
-  }
+  };
 
   componentDidUpdate(prevProps) {
     const { error, alert, message } = this.props;
@@ -17,10 +16,14 @@ export class Alerts extends Component {
       if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
       if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
       if (error.msg.message) alert.error(`Email: ${error.msg.message.join()}`);
+      if (error.msg.non_field_errors) alert.error(error.msg.non_field_errors.join());
+      if (error.msg.username) alert.error(error.msg.username.join());
+
     }
     if (message !== prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead);
-      if (message.addLead) alert.success(message.addLead)
+      if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
   }
 
